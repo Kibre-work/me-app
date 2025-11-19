098db1dad5f380f19123cd07ba54c17eec0df35a
Senait Agumasie <senaitagumasie1@gmail.com>
2025-11-19 23:48:03 +0400

    fix: motion lib issues fix

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-//import { motion } from "framer-motion";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 import axios from "axios";
 
 export default function Contact() {
